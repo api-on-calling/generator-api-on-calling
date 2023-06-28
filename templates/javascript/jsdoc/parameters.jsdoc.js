2b66834dcdc6e126a87e219643483404da17bd5a
peterlevel1 <3247482424@qq.com>
2023-06-29 02:11:59 +0800

    fix: enum optional

--- templates/javascript/jsdoc/parameters.jsdoc.js
+++ templates/javascript/jsdoc/parameters.jsdoc.js
@@ -8,7 +8,7 @@ module.exports = jsdocParameters;
  * @param {boolean} parameters[].required
  * @param {object} parameters[].schema
  * @param {('string' | 'boolean' | 'integer')} parameters[].schema.type
- * @param {string[] | number[]} parameters[].schema.enum
+ * @param {string[] | number[]} [parameters[].schema.enum]
  * @returns {string[]}
  */
 function jsdocParameters(parameters) {
